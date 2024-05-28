d2c5347bc098eba5258bb2528c755de87d06a364
htegeth <holger.tegethoff@gmail.com>
2024-05-28 11:16:21 +0200

    Einfaches Beispiel

--- src/main.cpp
+++ src/main.cpp
@@ -8,23 +8,14 @@
 #define CONTROL_LED PB3
 #define PCINT_VECTOR PCINT0_vect      // This step is not necessary - it's a naming thing for clarit
 
-// All times in microseconds
-#define Min_Gap_Time 5000 // nominal = 5600
-#define Max_Gap_Time 6200 // nominal = 5600
-#define One_Bit_Time 120 // nominal = 188
-#define Zero_Bit_Time 500 // nominal = 564
-
 
 boolean volatile irFired=false;
 boolean lightsOn =false;
 unsigned long startTime = 0;
-int pulseWidth;
+
 byte started = false;
 
-byte RF_Bit_Count;
-byte RF_Byte_Count;
-byte RF_Byte = 0;
-byte RF_Message[] = {0, 0, 0}; // 24 bits
+
 
 
 
@@ -39,77 +30,42 @@ void setup() {
   sei();                            //last line of setup - enable interrupts after setup
 }
 
-// This is the interrupt handler called when there is any change on the INT_PIN
-// ISR is defined in the headers - the ATtiny85 only has one handler
-ISR(PCINT_VECTOR)
-{ 
-  
-   if (digitalRead(INT_PIN) == LOW) {
-    startTime = micros();
-  }else{
-     if (startTime != 0) {
-      //calculate the pulse width
-      pulseWidth = ((volatile int)micros() - startTime);
-      //clear the timer
-      startTime = 0;
-
-      // Now check for valid message
-      if (pulseWidth < Max_Gap_Time) {
-        if (started == true) {
-          if (pulseWidth > Zero_Bit_Time) {
-            // bit = 0
-            RF_Bit_Count--; // bits received MSB first
-            bitClear(RF_Message[RF_Byte_Count], RF_Bit_Count);
-            if (RF_Bit_Count == 0) {
-              RF_Byte_Count++;
-              RF_Bit_Count = 8;
-            }
-          }
-          else if (pulseWidth > One_Bit_Time) {
-            // bit = 1
-            RF_Bit_Count--; // bits received MSB first
-            bitSet(RF_Message[RF_Byte_Count], RF_Bit_Count);
-            if (RF_Bit_Count == 0) {
-              RF_Byte_Count++;
-              RF_Bit_Count = 8;
-            }
-          }
-          else { // bad bit
-            started = false;
-          }
-        } // Started
-        
-        else if (pulseWidth > Min_Gap_Time) {
-          // start of message
-          started = true;
-          RF_Byte_Count = 0;
-          RF_Bit_Count = 8;
-        }
-      }
-      else { // invalid message
-        started = false;
-      }
-
-
-     }
+void blinkControll(int times, int  unsigned frequence)
+{
+  unsigned long start =  micros(); 
+  for(int i=0; i< times;i++){
+    digitalWrite(CONTROL_LED, HIGH);
+    while (micros()-start < frequence);
+    digitalWrite(CONTROL_LED, LOW);
   }
 
 }
 
+// This is the interrupt handler called when there is any change on the INT_PIN
+// ISR is defined in the headers - the ATtiny85 only has one handler
+ISR(PCINT_VECTOR)
+{ 
+   if (digitalRead(INT_PIN) == LOW) {
+    blinkControll(3,50);
+   }else{
+    blinkControll(3,150);
+   }
+}
+
+
+
+
 void loop() {  
-  if (RF_Byte_Count > 1)
-  {
-    cli(); 
-    if(lightsOn)
-    {         
-      digitalWrite(CONTROL_LED, LOW); 
-      lightsOn=false;
-    }else{
-      digitalWrite(CONTROL_LED, HIGH);
-      lightsOn=true; 
-    }
-    sei(); 
+  cli(); 
+  if(lightsOn)
+  {         
+    digitalWrite(CONTROL_LED, LOW); 
+    lightsOn=false;
+  }else{
+    digitalWrite(CONTROL_LED, HIGH);
+    lightsOn=true; 
   }
+  sei(); 
   digitalWrite(LED_PIN, HIGH);
   delay(200);
   digitalWrite(LED_PIN, LOW);
